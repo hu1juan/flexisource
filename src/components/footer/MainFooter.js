cf8a134b88722402caef9baa329ce42e466ab4d5
john mangmang <johnjeffritz@gmail.com>
2020-05-21 09:22:55 +0800

    added mobile menu styles

--- src/components/footer/MainFooter.js
+++ src/components/footer/MainFooter.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLinks from '../header/NavLinks';
 import logo from '../../assets/adrenalin.svg';
 import './footer.scss';
+import { MenuOutlined } from '@ant-design/icons';
 
 const MainFooter = () => {
   const menus = ['Privacy', 'Sitemap', 'Facebook', 'LinkedIn', 'Instagram', 'Twitter'];
+  const [showMenu, setShowMenu] = useState(true);
 
   return (
     <div className="foot-container">
       <img src={logo} alt='FlexiSource IT' />
-      <NavLinks menus={menus} />
+      <MenuOutlined onClick={() => setShowMenu(!showMenu)} />
+      {
+        showMenu ? < NavLinks menus={menus} /> : null
+      }
     </div>
   );
 }
--- src/components/header/MainNav.js
+++ src/components/header/MainNav.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLinks from './NavLinks';
 import logo from '../../assets/adrenalin.svg';
 import './header.scss';
-
+import { MenuOutlined } from '@ant-design/icons';
 const MainNav = () => {
   const menus = ['Culture', 'Work', 'Clients', 'Services', 'Careers', 'Contact'];
-
+  const [showMenu, setShowMenu] = useState(true);
   return (
     <div className="nav-container">
       <img src={logo} alt='FlexiSource IT' />
-      <NavLinks menus={menus} />
+      <MenuOutlined onClick={() => setShowMenu(!showMenu)} />
+      {
+        showMenu ? < NavLinks menus={menus} /> : null
+      }
     </div>
   );
 }
